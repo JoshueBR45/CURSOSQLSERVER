37e0fedb3f2660cf9982f7f0486ce94bc348538d
JoshueBR45 <143678238+JoshueBR45@users.noreply.github.com>
2024-07-03 08:02:23 +0200

    Indices columanres

---
+++ INDICE COLUMNARES .sql
@@ -0,0 +1,25 @@
+use Ciudadano
+go 
+
+-- NUmero de Gesitro de ciudadano 
+Select COUNT (*) from Ciudadano
+go
+
+-- hacer Una consulta a Ciudadno 
+Select NOM1, NOM2, APE1, APE2 FROM Ciudadano
+WHERE APE1= 'CARDENAS' AND APE2='VALENZUELA'
+GO
+
+--CREACION DE UN INDICE COLUMNAR
+Create clustered COLUMNSTORE INDEX CL_CIUDADANO 
+ON CIUDADNO;
+
+--Liberar la cache 
+DBCC FREEPRACCACHE WITH NO_INFOMSGS;
+DBCC DROPCLEANBUFFERS WITH NO_INFOMSGS;
+
+
+Drop index CL_CIUDADANO
+ON CIUDADNO
+
+SP_HELPINDEX CIUDADNO;
